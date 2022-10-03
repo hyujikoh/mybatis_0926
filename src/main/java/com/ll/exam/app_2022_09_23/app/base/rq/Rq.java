fc19d1f4663adff8c7eca533de8a055097be29e2
hyujikoh <dvum0045@gmail.com>
2022-10-03 21:19:27 +0900

    @SessionScope//브라우저 당 1개 하지만 어떤 브라우저를 선택해도, 인터셉터는 여러개가 만들어진게 아니라 하나로 통해진다. 내부 프록시가 잘 돌아간다.

--- src/main/java/com/ll/exam/app_2022_09_23/app/base/rq/Rq.java
+++ src/main/java/com/ll/exam/app_2022_09_23/app/base/rq/Rq.java
@@ -2,8 +2,12 @@ package com.ll.exam.app_2022_09_23.app.base.rq;
 
 import lombok.Getter;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Component
+//@ApplicationScope// 스프링부트 앱 당 1개
+@SessionScope//브라우저 당 1개
 public class Rq {
     @Getter
     private int count;
